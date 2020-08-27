d1a9e19722b613f7453d1aa19ea9503db8ff28ee
Misha Merkushin <merkushin.m.s@gmail.com>
2020-08-27 10:18:30 +0300

    feat: import main content

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/* =================================
+------------------------------------
+	Civic - CV Resume
+	Version: 1.0
+ ------------------------------------ 
+ ====================================*/
+
+
+
+'use strict';
+
+
+$(window).on('load', function() { 
+	/*------------------
+		Preloder
+	--------------------*/
+	$(".loader").fadeOut(); 
+	$("#preloder").delay(400).fadeOut("slow");
+
+});
+
+
+(function($) {
+
+	/*------------------
+		Background set
+	--------------------*/
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
+
+})(jQuery);
+
